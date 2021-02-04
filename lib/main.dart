8d6a7e1c2988ed5f806080b6de5eb45f7211fc51
roningrum <rizkioktavianingrum@gmail.com>
2021-02-04 14:00:49 +0700

    list widget array

--- lib/main.dart
+++ lib/main.dart
@@ -26,48 +26,39 @@ class MyApp extends StatelessWidget {
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
       ),
-      home: PageList(),
+      home: PageListWidget(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class PageList extends StatelessWidget {
+class PageListWidget extends StatefulWidget {
+  @override
+  _PageListWidgetState createState() => _PageListWidgetState();
+}
+
+class _PageListWidgetState extends State<PageListWidget> {
+  List<int> item = new List();
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 25; i++) {
+      item.add(i);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new ListView(
-        children:<Widget>[
-          new ListTile(
-            title: Text('Rendang'),
-           trailing: Icon(Icons.fastfood),
-          ),
-          new ListTile(
-            title: Text('Minuman'),
-            trailing: Icon(Icons.local_drink),
-          ),
-          new ListTile(
-            title: Text('Phone'),
-            trailing: Icon(Icons.phone),
-          ),
-          new ListTile(
-            title: Text('Alarm'),
-            trailing: Icon(Icons.alarm),
-          ),
-          new ListTile(
-            title: Text('Camera'),
-            trailing: Icon(Icons.camera),
-          )
-        ],
-      ),
+      body: new ListView.builder(
+          itemCount: item.length,
+          itemBuilder: (BuildContext context, int index) {
+            return new ListTile(
+              title: new Text('Item Nomor : $index'),
+              trailing: new Icon(Icons.print),
+            );
+          }),
     );
   }
 }
-
-
-
-
-
-
-
-
